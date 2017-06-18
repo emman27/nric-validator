64340210fb64f022b4d57e73574664ce695175cd
Emman <eygohlolz@gmail.com>
2017-06-18 23:47:42 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public'));
+app.use(require('./controllers'));
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000...');
+});
