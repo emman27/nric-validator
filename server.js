7d729adc8580fae64f4faab1848af548f5889ade
Emman <eygohlolz@gmail.com>
2017-06-18 23:52:03 +0800

    Update port

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 app.use(require('./controllers'));
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('Listening on port 3000...');
 });
