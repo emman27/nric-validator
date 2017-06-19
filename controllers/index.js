bd009927f3072c56e7a8e3d35aaa5d46d7966a25
Emman <eygohlolz@gmail.com>
2017-06-20 00:47:52 +0800

    Redirect to API docs

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,7 @@ const express = require('express'),
   router = express.Router();
 
 router.use('/api', require('./api.js'));
+router.get('/', (req, res) => {res.redirect('http://docs.nricvalidator.apiary.io/#');});
 // router.use('/', require('./default.js'));
 
 module.exports = router;
